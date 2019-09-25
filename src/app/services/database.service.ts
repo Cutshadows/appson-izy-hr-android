f110bc6c520dddb3da21bb9b992c5048534ba1bd
Douglas <ramnstein_1749_@hotmail.com>
2019-09-25 18:22:44 -0300

    dev login izyhrteam
    Better usability of source  code and distribution of service

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -9,22 +9,53 @@ import { delay } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class DatabaseService {
+  response:any;
   header: any = { 
     "headers": {
       "Content-Type": "application/json",
-      "Authorization": "BE6JVujuYvtWCSilKrRF1A1Rc+Zeyl4dZOG2VCWm9Uk="
+      "Authorization":"BE6JVujuYvtWCSilKrRF1A1Rc+Zeyl4dZOG2VCWm9Uk="
     } 
   }  
 
   constructor(public http: HttpClient,){}
   validateLogin(url, params){
-    let Promesa=new Promise((resolve, reject)=>{
+    let Promesa=new Promise((resolve)=>{
       this.http.post(url, params, this.header).subscribe((response)=>{
-        console.log(response);
+        let jsonRespond={
+          status,
+          response
+        }
+        if(response && response['status']==1){
+          jsonRespond.status="200";
+          jsonRespond.response=response;
+          this.response=jsonRespond;
+          resolve(jsonRespond);
+        }else if(response && response['status']==0){
+          jsonRespond.status="400";
+          jsonRespond.response=response;
+          this.response=jsonRespond;
+          resolve(this.response);
+        }
       },
       (error)=>{
-        console.log(error);
-      })
+        for (const key in error) {
+          switch(key){
+             case 'status':
+                if (error.hasOwnProperty(key)) {
+                  const element = error[key];
+                  if(element==0){
+                    let jsonResponseError={
+                      status
+                    }
+                    jsonResponseError.status="0";
+                    this.response=jsonResponseError;
+                    resolve(this.response);
+                  }
+                } 
+              break;
+          }
+        }
+      });
     })
     return Promesa;
     
